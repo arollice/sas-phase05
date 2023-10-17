8e3de608ffaabc733cc7de02ff9841294a2ea499
Ashlee Rollice <ashleerollice@MB-P.local>
2023-10-17 13:22:18 -0400

    adds url in table

--- phase01/public/salamanders/index.php
+++ phase01/public/salamanders/index.php
@@ -10,11 +10,12 @@ id=4,  salamanderName= Slimy Salamander
 -->
 <?php
 $salamanders = [
-
+['id'=>'1', 'salamanderName' => 'Red-Legged Salamander'],
+['id'=>'2', 'salamanderName' => 'Pigeon Mountain Salamander'],
+['id'=>'3', 'salamanderName' => 'ZigZag Salamander'],
+['id' => '4', 'salamanderName' => 'Slimy Salamander'],
 ];
 ?>
-
-
 
 <!-- Add the pageTitle for salamanders
  & Include a shared path to the salamander header -->
@@ -24,7 +25,6 @@ $pageTitle = 'Salamanders';
 
 include(SHARED_PATH . '/salamander-header.php');
 ?>
-
 
 <h1>Salamanders</h1>
 
@@ -42,11 +42,14 @@ include(SHARED_PATH . '/salamander-header.php');
       <?php foreach($salamanders as $salamander) { ?>
         <tr>
           <!-- <td>Display the salamander id</td> -->
-          <td></td>
+          <td><?php echo h($salamander['id']); ?></td>
     	    <!-- <td>Display the salamander name</td> -->
-          <td></td>
+          <td><?php echo h($salamander['salamanderName']); ?></td>
+          
           <!-- Use urlFor with show.php AND h(u) with the salamander['id'] -->
-          <td></td>
+          
+          <td><a href="<?php urlFor('/salamanders/show.php?id=' . h(u($salamander['id']))); ?>">View</a></td>
+
           <td><a href="#">Edit</a></td>
           <td><a href="#">Delete</a></td>
     	  </tr>
