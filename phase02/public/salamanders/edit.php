673c313ea1c9a08f145c83ca9ff11eb07c855a1f
Ashlee Rollice <ashleerollice@MB-P.local>
2023-10-24 14:25:27 -0400

    Taking Break

---
+++ phase02/public/salamanders/edit.php
@@ -0,0 +1,21 @@
+<?php require_once('../../private/initialize.php');
+
+$pageTitle = 'Salamander Details';
+
+include(SHARED_PATH . '/salamander-header.php')
+?>
+<p><a href="<?= url_for('/salamanders/index.php'); ?>">&laquo; Back to Salamander List</a></p>
+
+<h1>Edit Salamander</h1>
+<form>
+  <label for = "name">Name:</label>
+  <br>
+  <input type="text" name ="Name">
+  <br>
+  <input type = "submit" value="Edit Salamander">
+</form>
+
+
+<?php
+include(SHARED_PATH . '/salamander-footer.php')
+?>
--- phase02/public/salamanders/new.php
+++ phase02/public/salamanders/new.php
@@ -1,18 +1,15 @@
 <?php 
-
 require_once('../../private/initialize.php');
 
-$test = $GET_['test'] ?? '';
+$test = $_GET['test'] ?? '';
 
 if($test == '404')  {
  error_404(); 
-} else if ($test == '500'){
+} 
+else if ($test == '500'){
   error_500();
-  }
-  else if ($test == 'redirect') {
-    redirect_to(url_for('phase02/public/salamanders/index.php'));
-  }
- else {
-  echo 'No error';
+}
+else if ($test == 'redirect') {
+  redirect_to(url_for('/salamanders/index.php'));
 }
 ?>
