c1642beb45308ebfd6627e760307da8ba313aa32
Ashlee Rollice <ashleerollice@MB-P.local>
2023-11-28 08:48:32 -0500

    DB is up

--- phase04/public/salamanders/index.php
+++ phase04/public/salamanders/index.php
@@ -35,6 +35,6 @@ include(SHARED_PATH . '/salamander-header.php');
       <?php } ?>
   	</table>
   <?php mysqli_free_result($salamander_set); ?>
-  Thanks to <a href="https://herpsofnc.org">Ampibians and Reptiles of North Carolina</a>
+  Thanks to <a href="https://herpsofnc.org">Amphibians and Reptiles of North Carolina</a>
 
 <?php include(SHARED_PATH . '/salamander-footer.php'); ?>
