f54d3e97f21c36d79490f13af2320681e0766308
Ashlee Rollice <ashleerollice@MB-P.local>
2023-11-29 08:31:22 -0500

    Deletes salamander from database

--- phase04/public/salamanders/delete.php
+++ phase04/public/salamanders/delete.php
@@ -1,25 +1,50 @@
-<?php require_once('../../private/initialize.php'); 
+<?php 
+require_once('../../private/initialize.php'); 
 include(SHARED_PATH . '/salamander-header.php');
+
 if(!isset($_GET['id'])) {
     redirect_to(url_for('salamanders/index.php'));
   }
-  $id = $_GET['id'];
+    $id = $_GET['id'];
+   
+    
+    $salamander = find_salamander_by_id($id);
+    
+    if(is_post_request()) {
+      $sql = "DELETE FROM salamander ";
+      $sql .= "WHERE id='".$id."' ";
+      $sql .= "LIMIT 1";
+
+      $result = mysqli_query($db, $sql);
+
+       //For DELETE statements, $result is true/false
+    if($result){
+
+      redirect_to(url_for('salamanders/index.php'));
+    } else {
+
+      //Delete failed 
+      echo mysqli_error($db);
+      db_disconnect($db);  
+      exit;
+    }
+     
+   }
   
-  $salamander = find_salamander_by_id($id);
-  
-  if(is_post_request()) {
-  
-  }
-  
-  $pageTitle = 'Delete Salamander'; ?>
-  
-    <a href="<?php echo url_for('salamanders/index.php'); ?>">&laquo; Back to Salamanders</a>
+  $pageTitle = 'Delete Salamander'; 
+  ?>
+
+   <div>
       <h1>Delete Salamander</h1>
+      <a href="<?php echo url_for('salamanders/index.php'); ?>">&laquo; Back to Salamanders</a>
+
       <p>Are you sure you want to delete this salamander?</p>
       <p><?php echo h($salamander['name']); ?></p>
   
-      <form action="<?php echo url_for('salamanders/delete.php?id=' . h(u($salamander['id']))); ?>" method="post">
-          <input type="submit" name="commit" value="Delete Salamander" />
+      <form action="<?php echo url_for('salamanders/delete.php?id='.h(u($salamander['id']))); ?>" method="post">
+          <input type="submit" name="commit" value="Delete Salamander">
       </form>
+      
+  </div>
 
 <?php include(SHARED_PATH . '/salamander-footer.php'); ?>
